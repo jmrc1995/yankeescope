2d0a0e6952c6a997f1c522d55ec1fe1c1b04c90e
Jomar Cardoza <jomar.cardoza95@gmail.com>
2025-06-02 13:03:56 -0400

    Ensure express server routes updated and stable for stat requests

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,16 +2,17 @@ import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import playersRoute from './routes/players';
-
-
-const app = express();
+import mlbRoutes from './routes/mlb';
 
 dotenv.config(); 
 
-
+const app = express();
 app.use(cors());
-app.use('/players', playersRoute); 
 app.use(express.json());
+
+app.use('/players', playersRoute); 
+app.use('/mlb', mlbRoutes);
+
 
 app.get('/', (_req, res) => {
   res.send('YankeeScope API is live ⚾');
